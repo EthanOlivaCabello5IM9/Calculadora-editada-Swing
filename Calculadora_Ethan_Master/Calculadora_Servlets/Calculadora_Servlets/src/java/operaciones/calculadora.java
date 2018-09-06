8ac359e2b614f60d70d8c0ca9960087a6d07b336
EthanOlivaCabello5IM9 <42345569+EthanOlivaCabello5IM9@users.noreply.github.com>
2018-09-06 08:32:25 -0500

    chido
    @emmanuel789

---
+++ Calculadora_Ethan_Master/Calculadora_Servlets/Calculadora_Servlets/src/java/operaciones/calculadora.java
@@ -0,0 +1,73 @@
+package operaciones;
+
+import   java.io.*;
+import   javax.servlet.*;
+import   javax.servlet.http.*;
+ 
+public   class   calculadora  extends   HttpServlet
+{
+public   void   doPost(HttpServletRequest   req,   HttpServletResponse res)
+throws   ServletException,   IOException
+    {
+    double  n1,n2,result;
+    int     operacion;
+
+    ServletOutputStream out = res.getOutputStream();
+
+    n1   =   Double.parseDouble(req.getParameter("num1"));
+    n2   =   Double.parseDouble(req.getParameter("num2"));
+    operacion = Integer.parseInt(req.getParameter("ope"));
+
+    result   =   calcularRes(n1,n2,operacion);
+
+    out.println("<html>");
+    out.println("<head><title>.:CalculadoraServlet:.</title></head>");
+    out.println("<body>");
+    out.println("<h1><center>El resultado es</center></h1>");
+    out.println("<h2>   <b><center>"+  result   +"</center></b></h2>");
+    out.println("<a href=\"index.html\">Regresar</a>");
+    out.println("</body>");
+    out.println("</html>");
+    out.close();
+    }
+
+    public   double   calcularRes(double   n1,   double   n2,   int   operacion)
+    {
+    double   result=0;
+
+        switch(operacion)
+        {
+            case 1:
+            return n1 + n2;
+            
+        case   2:
+            return   n1 - n2;
+            
+        case   3:
+            return   n1 * n2;
+            
+        case   4:
+            return   n1 / n2;
+            
+        case 5:
+            return  Math.abs(n1);
+        case 6:
+            return  Math.sin(n1);
+            
+        case 7:
+            return  Math.cos(n1);
+            
+        case 8:
+            return  Math.tan(n1);
+            
+        case 9:
+            return   Math.pow(n1,2);
+        
+        case 10:
+            return  Math.log10(n1);
+        }
+
+    return   result;
+    }
+}
+ 
